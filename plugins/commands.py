d29afe880d3007ab89035004b348bd45d9ae30e7
MRALLMOVIES <132032612+MRALLMOVIES@users.noreply.github.com>
2024-07-06 18:07:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -105,7 +105,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=START_MESSAGE.format(user=message.from_user.mention, bot=temp.B_LINK),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
+            parse_mode=enums.ParseMode.MARKDOWN
         )
         return
     data = message.command[1]
